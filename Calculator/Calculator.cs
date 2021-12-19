2bef5dd0cebcd6db5bba352c847f966fd02f5097
JacquesPhilippe Amiot <jacquesphilippe.amiot@zimmercas.com>
2021-12-18 19:47:52 -0500

    Create basic calculator app
    Created basic calculator console application.
    Solution is made up of three projects:
    |
    |--- Calculator class library
    |--- Calculator.Test xUnit project
    |--- Console application consuming calculator class lib
    
    The purpose of this project is to try and learn how to use Azure
    Pipelines. You have access to all features, and it's free if you use
    Github, so that's what I'm trying to do.

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CalculatorLib
+{
+
+
+    /// <summary>
+    /// A calculator library
+    /// </summary>
+    public class Calculator
+    {
+        /// <summary>
+        /// Addition function; returns the sum of the arguments
+        /// </summary>
+        /// <param name="arg1"></param>
+        /// <param name="arg2"></param>
+        /// <returns></returns>
+        public float Add(float arg1, float arg2) => arg1 + arg2;
+
+        /// <summary>
+        /// Subtraction function; returns the difference of the arguments
+        /// </summary>
+        /// <param name="arg1"></param>
+        /// <param name="arg2"></param>
+        /// <returns></returns>
+        public float Subtract(float arg1, float arg2) => arg1 - arg2;
+    }
+}
